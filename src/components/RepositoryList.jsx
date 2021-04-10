32965973feb2df31faaf6fee5dc64df9b45b4c50
Paulo Vinicius <ploviniciusplo@gmail.com>
2021-04-10 13:36:59 -0300

    feat: Utilizando o hook useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,7 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
+
 
 const repository = {
   name: 'unform',
@@ -8,6 +10,14 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositorios</h1>
